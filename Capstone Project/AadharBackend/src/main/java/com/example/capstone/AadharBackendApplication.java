7dfc5ce92dda112c48dd4acb9e437d9745ade53b
sabdulwahid1 <sabdulwahidsw@gmail.com>
2024-02-21 19:14:57 +0530

    Capstone Project

---
+++ Capstone Project/AadharBackend/src/main/java/com/example/capstone/AadharBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadharBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadharBackendApplication.class, args);
+		System.err.println("Spring Boot is Up...");
+	}
+
+}
